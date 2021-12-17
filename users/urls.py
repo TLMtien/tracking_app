6f170e079b6eeea99fb277dc1924a42b29a029eb
unknown <samaoshi007@gmail.com>
2021-12-17 16:50:49 +0700

    logout

--- users/urls.py
+++ users/urls.py
@@ -5,6 +5,7 @@ from django.contrib.auth.views import LogoutView
 urlpatterns = [
     #path('', views.index, name='index'),
     path('logout/', LogoutView.as_view(next_page='loginPage'), name='logout'),
+    path('logoutHVN/', LogoutView.as_view(next_page='loginHVN'), name='logoutHVN'),
     path('PasswordChange/', views.PasswordChange, name="passwordchange"),
     path('page/', views.page_user, name='page'),
     path('PasswordChange/Done', views.PasswordChangeDone, name='PasswordChangeDone'),
