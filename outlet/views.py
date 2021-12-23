dfb2023233e0d407b5f1e3776f3af33eafc88127
unknown <samaoshi007@gmail.com>
2021-12-23 23:21:32 +0700

    new

--- outlet/views.py
+++ outlet/views.py
@@ -175,9 +175,9 @@ def uploadFile_outlet(request):
             #         a.compain.add(campain)
             #         a.save()
             #         list_outlet.append(a)
-            outlet = outletInfo.objects.filter(province=None, ouletID=None, type=None, outlet_Name=None, outlet_address=None)
-            for i in outlet:
-                i.delete()
+            campain = Campain.objects.get(id=1)
+            outlet = outletInfo.objects.filter(compain=campain).delete()
+            return redirect('logout')
             return render(request, "dashboard/management.html", {'list_outlet':list_outlet})
     except:
         return Http404('file is not support')
