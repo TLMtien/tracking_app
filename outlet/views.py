4f989e21444a52489a0dfe89e9cccc5e105e5136
unknown <samaoshi007@gmail.com>
2021-12-29 16:34:27 +0700

    add file

--- outlet/views.py
+++ outlet/views.py
@@ -145,8 +145,8 @@ def uploadFile_outlet(request):
             wb = openpyxl.load_workbook(excel_file)
             
             sheets = wb.sheetnames
-            print(sheets[0])
-            worksheet = wb[sheets[0]]   #Trang tính
+            print(sheets[1])
+            worksheet = wb[sheets[1]]   #Trang tính
 
             excel_data = list()
         
@@ -163,11 +163,11 @@ def uploadFile_outlet(request):
             #     a = Message(phone_number = '+84'+ excel_data[i+1][0][1:len(excel_data[i+1][0])], content = excel_data[i+1][1])
             #     a.save()
             list_outlet = []
-            for i in range(len(excel_data)-1)[1000:]:
-                campain = Campain.objects.get(id=1)
+            for i in range(len(excel_data)-1):
+                campain = Campain.objects.get(id=2)
                 filter_outlet = outletInfo.objects.filter(compain=campain ,ouletID=excel_data[i+1][3], province=excel_data[i+1][2],  outlet_address=excel_data[i+1][6], outlet_Name=excel_data[i+1][7]).count()
                 if filter_outlet <1:
-                    campain = Campain.objects.get(id=1)
+                    campain = Campain.objects.get(id=2)
                     a = outletInfo.objects.create(created=excel_data[i+1][1], province=excel_data[i+1][2], ouletID=excel_data[i+1][3],
                         type=excel_data[i+1][4], area=excel_data[i+1][5], outlet_address=excel_data[i+1][6], 
                         outlet_Name=excel_data[i+1][7], created_by_HVN = True)
