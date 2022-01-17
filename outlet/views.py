beda40b38ee2b385b9c74883dab13f518935f163
unknown <samaoshi007@gmail.com>
2022-01-17 16:48:44 +0700

    upload outlet

--- outlet/views.py
+++ outlet/views.py
@@ -168,7 +168,7 @@ def uploadFile_outlet(request, campainID):
             for i in range(len(excel_data)-1):
                 campain = Campain.objects.get(id=campainID)
                 #filter_outlet = outletInfo.objects.filter(compain=campain ,ouletID=excel_data[i+1][3], province=excel_data[i+1][2],  outlet_address=excel_data[i+1][6], outlet_Name=excel_data[i+1][7]).count()
-                filter_outlet = outletInfo.objects.filter(compain=campain ,ouletID=excel_data[i+1][3]).count()
+                filter_outlet = outletInfo.objects.filter(compain=campain ,ouletID=excel_data[i+1][3], created_by_HVN = True).count()
                 if filter_outlet <1:
                     campain = Campain.objects.get(id=campainID)
                     a = outletInfo.objects.create(created=excel_data[i+1][1], province=excel_data[i+1][2], ouletID=excel_data[i+1][3],
