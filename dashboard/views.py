17efdad0aaf52882d97314afc4e0ae4f76be1e0d
unknown <samaoshi007@gmail.com>
2022-01-17 10:53:01 +0700

    outlet approval

--- dashboard/views.py
+++ dashboard/views.py
@@ -358,10 +358,12 @@ def edit_outlet_approval(request):
                 outlet_info.outlet_Name = outlet_Name[i]
                 outlet_info.outlet_address = outlet_address[i]
                 #print(outlet_info.created_by_SP)
-                sale_person = SalePerson.objects.filter(brand__pk=8)
+                sale_person = SalePerson.objects.filter(brand__pk=4)
                 print(sale_person[i].user)
                 if outlet_info.created_by_SP == None:
-                    outlet_info.created_by_SP = sale_person[i].user
+                    for j in range(len(sale_person)):
+                        outlet_info.created_by_SP = sale_person[j].user
+                        break
                 # rp_table = tableReport.objects.filter(outlet=outlet_info)
                 # count_rp_table = tableReport.objects.filter(outlet=outlet_info).count()
                 # count_rp_sale =  report_sale.objects.filter(outlet=outlet_info).count()
