029fd41f0e6ab69032e38d05058631ea492e7d17
unknown <samaoshi007@gmail.com>
2022-01-17 11:52:54 +0700

    KPI

--- dashboard/charts.py
+++ dashboard/charts.py
@@ -156,8 +156,9 @@ def VOLUME_PERFORMANCE(campain_id, all_outlet, from_date, to_date):
     count_kpi = KPI.objects.filter(campain=Cp).count()
     if count_kpi > 0:
         new_kpi =  KPI.objects.filter(campain=Cp)[count_kpi-1:]
-        volume_achieved = new_kpi.volume_achieved
-        volume_achieved = int(volume_achieved)
+        for kpi in new_kpi:
+            volume_achieved = kpi.volume_achieved
+            volume_achieved = int(volume_achieved)
     # all_outlet = outletInfo.objects.filter(compain=Cp)
     count = 0
     count_volume_achieved = 0
